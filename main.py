1cf49c26c497f139c870a17f4c06284ad4304c8e
nuzcraft <nuzcraft@gmail.com>
2022-02-13 14:41:32 -0600

    adding clear command to clear the console after presenting it

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ def main() -> None:
         while True:
             root_console.print(x=player_x, y=player_y, string="@")
             context.present(root_console)
+            root_console.clear()
             for event in tcod.event.wait():
                 action = event_handler.dispatch(event)
 
