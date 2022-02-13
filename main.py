4bd488112110527a4a93b755a7b8c07cf5622c88
nuzcraft <nuzcraft@gmail.com>
2022-02-13 14:40:35 -0600

    updating main to take player inputs and move @

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import tcod
 
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
+
 
 def main() -> None:
     screen_width = 80
@@ -12,6 +15,8 @@ def main() -> None:
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
+
+    event_handler = EventHandler()
 
     with tcod.context.new_terminal(
         screen_width,
@@ -25,7 +30,15 @@ def main() -> None:
             root_console.print(x=player_x, y=player_y, string="@")
             context.present(root_console)
             for event in tcod.event.wait():
-                if event.type == "QUIT":
+                action = event_handler.dispatch(event)
+
+                if action is None:
+                    continue
+
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+                elif isinstance(action, EscapeAction):
                     raise SystemExit()
 
 
