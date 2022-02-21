0850c32d2c891aab4441fc688cbcff47c40a2cc4
nuzcraft <nuzcraft@gmail.com>
2022-02-21 07:30:55 -0600

    added generate_dungeon and incorporated it into main file

--- Tests/test_procgen.py
+++ Tests/test_procgen.py
@@ -1,9 +1,12 @@
 import unittest
-from procgen import RectangularRoom
+from procgen import RectangularRoom, generate_dungeon
 
 
 class Test_RectangularRoom(unittest.TestCase):
     def test_init(self):
+        '''
+        test instantiation of a rectangle
+        '''
         x, y, w, h = 5, 6, 7, 8
         rm = RectangularRoom(x, y, w, h)
         self.assertEqual(rm.x1, x)
@@ -12,6 +15,9 @@ class Test_RectangularRoom(unittest.TestCase):
         self.assertEqual(rm.y2, y+h)
 
     def test_center_odd_sides(self):
+        '''
+        tests that the center property returns correctly for odd sides
+        '''
         x, y, w, h = 0, 0, 5, 5
         rm = RectangularRoom(x, y, w, h)
         # int will cut off the decimals
@@ -21,6 +27,9 @@ class Test_RectangularRoom(unittest.TestCase):
         self.assertEqual(rm.center[1], c_y)
 
     def test_center_even_sides(self):
+        '''
+        tests that the center property returns correctly for even sides
+        '''
         x, y, w, h = 0, 0, 6, 6
         rm = RectangularRoom(x, y, w, h)
         # int() will cut off the decimals
@@ -30,9 +39,24 @@ class Test_RectangularRoom(unittest.TestCase):
         self.assertEqual(rm.center[1], c_y)
 
     def test_inner(self):
+        '''
+        tests that the inner property returns correctly
+        '''
         x, y, w, h = 0, 0, 6, 6
         rm = RectangularRoom(x, y, w, h)
         # inner will return a tuple of slices
         # which excludes the lower bound
-        self.assertEqual(rm.inner[0], slice(x+1, x+w))
-        self.assertEqual(rm.inner[1], slice(y+1, y+h))
+        self.assertEqual(rm.inner[0], slice(1, 6))
+        self.assertEqual(rm.inner[1], slice(1, 6))
+
+
+class Test_Generate_Dungeon(unittest.TestCase):
+    def test_generate_dungeon(self):
+        '''
+        tests that generate_dungeon returns a map of the correct size
+        we don't care what the dungeon looks like, but this should test
+        calling it doesn't cause an error or misshape the result
+        '''
+        d = generate_dungeon(50, 50)
+        self.assertEqual(d.height, 50)
+        self.assertEqual(d.width, 50)
--- procgen.py
+++ procgen.py
@@ -1,5 +1,8 @@
 from typing import Tuple
 
+from game_map import GameMap
+import tile_types
+
 
 class RectangularRoom:
     def __init__(self, x: int, y: int, width: int, height: int):
@@ -19,3 +22,15 @@ class RectangularRoom:
     def inner(self) -> Tuple[slice, slice]:
         """Return the inner area of the room as a 2D array index"""
         return slice(self.x1 + 1, self.x2), slice(self.y1 + 1, self.y2)
+
+
+def generate_dungeon(map_width, map_height) -> GameMap:
+    dungeon = GameMap(map_width, map_height)
+
+    room_1 = RectangularRoom(x=20, y=15, width=10, height=15)
+    room_2 = RectangularRoom(x=35, y=15, width=10, height=15)
+
+    dungeon.tiles[room_1.inner] = tile_types.floor
+    dungeon.tiles[room_2.inner] = tile_types.floor
+
+    return dungeon
