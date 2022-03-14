81aea2bbdb56e98a80cd120bed6c73f9af5d9917
nuzcraft <nuzcraft@gmail.com>
2022-03-14 11:24:40 -0500

    stating to add components

---
+++ Tests/test_base_component.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestBaseComponent(unittest.testcase):
+    def test_property_engine(self):
+        '''
+        tests that the engine property returns
+        correctly
+        '''
