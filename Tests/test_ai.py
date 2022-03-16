72e947cb5bbd987e00b764fbb7e3b181c1cf989d
nuzcraft <nuzcraft@gmail.com>
2022-03-16 14:24:23 -0500

    finishing up get_path_to unit tests

--- Tests/test_ai.py
+++ Tests/test_ai.py
@@ -1,10 +1,12 @@
-from lib2to3.pytree import Base
 import unittest
+from typing import List, Tuple
 
 from components.ai import BaseAI
 from entity import Entity
 from game_map import GameMap
 from engine import Engine
+import tile_types
+
 
 class Test_BaseAI(unittest.TestCase):
     def test_perform(self):
@@ -19,19 +21,71 @@ class Test_BaseAI(unittest.TestCase):
 
     def test_get_path_to_straight_line(self):
         '''
-        how should we test this function?
-        let's do 3 tests
+        test get_path_to function
         1. test that it returns a short path in a straight line
-        2. test that it avoids walls
-        3. test that it avoids blocking entities
-        we don't really want to test the pathfinder, just that it works
-        with our walkable array + avoiding entities that block movement
         '''
-        ent = Entity()
+        ent = Entity(x=0, y=0)
         eng = Engine(player=ent)
         gm = GameMap(engine=eng, width=10, height=10)
+        # GameMaps are initialized as all wall, convert to floor
+        gm.tiles[:, :] = tile_types.floor
         ent.gamemap = gm
         ai = BaseAI(entity=ent)
+        #  ent starts at 0,0 - get the path straight down to 0,9 (bottom of map)
         path = ai.get_path_to(dest_x=0, dest_y=9)
-        self.assertEqual(len(path), 300)
+        # path should be...
+        # (0,1)...(0,9)
+        path_should_be: List[Tuple[int, int]] = [
+            (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (0, 8), (0, 9)]
+        self.assertEqual(path, path_should_be)
 
+    def test_get_path_to_avoid_wall(self):
+        '''
+        test get_path_to function
+        2. test that it avoids walls
+        '''
+        ent = Entity(x=0, y=0)
+        eng = Engine(player=ent)
+        gm = GameMap(engine=eng, width=10, height=10)
+        # GameMaps are initialized as all wall, convert to floor
+        gm.tiles[:, :] = tile_types.floor
+        # change a couple of the path tiles to a wall (unwalkable)
+        gm.tiles[0, 5] = tile_types.wall
+        gm.tiles[1, 5] = tile_types.wall
+        ent.gamemap = gm
+        ai = BaseAI(entity=ent)
+        #  ent starts at 0,0 - get the path straight down to 0,9 (bottom of map)
+        path = ai.get_path_to(dest_x=0, dest_y=9)
+        # straight path should be (0,1)...(0,9)
+        # but since (0,5) and (1, 5) are unwalkable,
+        # the path increases to go around them (diagonal to (1,4) and (2,5) then
+        # back to (1,6) before straightening out)
+        path_should_be: List[Tuple[int, int]] = [
+            (0, 1), (0, 2), (0, 3), (1, 4), (2, 5), (1, 6), (0, 7), (0, 8), (0, 9)]
+        self.assertEqual(path, path_should_be)
+
+    def test_get_path_to_avoid_entities(self):
+        '''
+        test get_path_to function
+        3. test that it avoids blocking entities
+        '''
+        ent = Entity(x=0, y=0)
+        eng = Engine(player=ent)
+        gm = GameMap(engine=eng, width=10, height=10)
+        # GameMaps are initialized as all wall, convert to floor
+        gm.tiles[:, :] = tile_types.floor
+        # add a couple entities to the gamemap that are in the way
+        ent1 = Entity(x=0, y=5, blocks_movement=True)
+        ent2 = Entity(x=1, y=5, blocks_movement=True)
+        gm.entities = {ent1, ent2}
+        ent.gamemap = gm
+        ai = BaseAI(entity=ent)
+        #  ent starts at 0,0 - get the path straight down to 0,9 (bottom of map)
+        path = ai.get_path_to(dest_x=0, dest_y=9)
+        # straight path should be (0,1)...(0,9)
+        # but since (0,5) and (1, 5) have blocking entities,
+        # the path increases to go around them (diagonal to (1,4) and (2,5) then
+        # back to (1,6) before straightening out)
+        path_should_be: List[Tuple[int, int]] = [
+            (0, 1), (0, 2), (0, 3), (1, 4), (2, 5), (1, 6), (0, 7), (0, 8), (0, 9)]
+        self.assertEqual(path, path_should_be)
