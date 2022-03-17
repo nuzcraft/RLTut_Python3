304864027a8fa0e7e9a53ec5006591c35619c224
nuzcraft <nuzcraft@gmail.com>
2022-03-17 12:29:26 -0500

    adding move_keys dictionary

--- input_handlers.py
+++ input_handlers.py
@@ -9,6 +9,35 @@ from actions import Action, EscapeAction, BumpAction
 if TYPE_CHECKING:
     from engine import Engine
 
+MOVE_KEYS = {
+    # arrow keys
+    tcod.event.K_UP: (0, -1),
+    tcod.event.K_DOWN: (0, 1),
+    tcod.event.K_LEFT: (-1, 0),
+    tcod.event.K_RIGHT: (1, 0),
+    tcod.event.K_HOME: (-1, -1),
+    tcod.event.K_END: (-1, 1),
+    tcod.event.K_PAGEUP: (1, -1),
+    tcod.event.K_PAGEDOWN: (1, 1),
+    # numpad keys
+    tcod.event.K_KP_8: (0, -1),
+    tcod.event.K_KP_2: (0, 1),
+    tcod.event.K_KP_4: (-1, 0),
+    tcod.event.K_KP_6: (1, 0),
+    tcod.event.K_KP_7: (-1, -1),
+    tcod.event.K_KP_1: (-1, 1),
+    tcod.event.K_KP_9: (1, -1),
+    tcod.event.K_KP_3: (1, 1),
+    # vi keys
+    tcod.event.K_k: (0, -1),
+    tcod.event.K_j: (0, 1),
+    tcod.event.K_h: (-1, 0),
+    tcod.event.K_l: (1, 0),
+    tcod.event.K_y: (-1, -1),
+    tcod.event.K_b: (-1, 1),
+    tcod.event.K_u: (1, -1),
+    tcod.event.K_n: (1, 1),
+}
 
 class EventHandler(tcod.event.EventDispatch[Action]):
     def __init__(self, engine: Engine):
