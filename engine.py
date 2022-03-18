4ef3e3eb9b6dafdc689b292f0cf4784e6ab97f96
nuzcraft <nuzcraft@gmail.com>
2022-03-18 16:22:21 -0500

    update engine to use the new maingameeventhandler

--- engine.py
+++ engine.py
@@ -6,18 +6,19 @@ from tcod.context import Context
 from tcod.console import Console
 from tcod.map import compute_fov
 
-from input_handlers import EventHandler
+from input_handlers import MainGameEventHandler
 
 if TYPE_CHECKING:
     from entity import Actor
     from game_map import GameMap
+    from input_handlers import EventHandler
 
 
 class Engine:
     game_map: GameMap
 
     def __init__(self, player: Actor):
-        self.event_handler: EventHandler = EventHandler(self)
+        self.event_handler: EventHandler = MainGameEventHandler(self)
         self.player = player
 
     def handle_enemy_turns(self) -> None:
