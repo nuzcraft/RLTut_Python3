6b2b588d4932880c653dee162c48e9a628d0af88
nuzcraft <nuzcraft@gmail.com>
2022-03-18 09:51:31 -0500

    significant update to event handler so that we can have a game over

--- input_handlers.py
+++ input_handlers.py
@@ -51,6 +51,13 @@ class EventHandler(tcod.event.EventDispatch[Action]):
         self.engine = engine
 
     def handle_events(self) -> None:
+        raise NotImplementedError
+
+    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
+        raise SystemExit()
+
+class MainGameEventHandler(EventHandler):
+    def handle_events(self) -> None:
         for event in tcod.event.wait():
             action = self.dispatch(event)
 
@@ -61,9 +68,6 @@ class EventHandler(tcod.event.EventDispatch[Action]):
 
             self.engine.handle_enemy_turns()
             self.engine.update_fov()  # update the fov before the player's next action
-
-    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
-        raise SystemExit()
 
     def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
         action: Optional[Action] = None
@@ -81,4 +85,25 @@ class EventHandler(tcod.event.EventDispatch[Action]):
         elif key == tcod.event.K_ESCAPE:
             action = EscapeAction(player)
 
+        return action
+
+class GameOverEventHandler(EventHandler):
+    def handle_events(self) -> None:
+        for event in tcod.event.wait():
+            action = self.dispatch(event)
+
+            if action is None:
+                continue
+
+            action.perform()
+
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
+        action: Optional[Action] = None
+
+        key = event.sym
+
+        if key == tcod.event.K_ESCAPE:
+            action = EscapeAction(self.engine.player)
+
+        # no valid key was pressed
         return action
