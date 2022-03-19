f39d736d319deb47aff68d305459efae1ed29c1c
nuzcraft <nuzcraft@gmail.com>
2022-03-19 17:54:09 -0500

    finished unit tests for render_bar

--- Tests/test_render_functions.py
+++ Tests/test_render_functions.py
@@ -1,17 +1,48 @@
 import unittest
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 import render_functions
+import tcod
+import color
+
 
 class Test_Render_Functions(unittest.TestCase):
-    def test_render_bar_value_greaterthan_0(self):
+    def test_render_bar_value_0(self):
         '''
         verify that the hp bar is rendered with 2 rectangles
         when the value is greater than 0
-        TODO: figure out how to mock the draw_rect calls in here
         '''
-        console = Mock()
-        render_functions.render_bar(
-            console=console, current_value=0, maximum_value=10, total_width=10
-        )
-        
+        console = tcod.Console(width=10, height=10)
+
+        with patch('tcod.Console.draw_rect') as patches:
+            with patch('tcod.Console.print') as patch_print:
+                render_functions.render_bar(
+                    console=console, current_value=0, maximum_value=10, total_width=10
+                )
+
+        patches.assert_called_once_with(
+            x=0, y=45, width=10, height=1, ch=1, bg=color.bar_empty)
+
+        patch_print.assert_called_once()
+
+    def test_render_bar_value_greater_than_0(self):
+        '''
+        verify that the hp bar is rendered with 2 rectangles
+        when the value is greater than 0
+        '''
+        console = tcod.Console(width=10, height=10)
+
+        with patch('tcod.Console.draw_rect') as patch_draw_rect:
+            with patch('tcod.Console.print') as patch_print:
+
+                render_functions.render_bar(
+                    console=console, current_value=5, maximum_value=10, total_width=10
+                )
+
+        patch_draw_rect.assert_any_call(
+            x=0, y=45, width=5, height=1, ch=1, bg=color.bar_filled)
+
+        patch_draw_rect.assert_any_call(
+            x=0, y=45, width=10, height=1, ch=1, bg=color.bar_empty)
+
+        patch_print.assert_called_once()
--- render_functions.py
+++ render_functions.py
@@ -9,16 +9,18 @@ import color
 if TYPE_CHECKING:
     from tcod import Console
 
+
 def render_bar(
     console: Console, current_value: int, maximum_value: int, total_width: int,
 ) -> None:
     bar_width = int(float(current_value) / maximum_value * total_width)
 
-    console.draw_rect(x=0, y=45, width=total_width, height=1, ch=1, bg=color.bar_empty)
+    console.draw_rect(x=0, y=45, width=total_width,
+                      height=1, ch=1, bg=color.bar_empty)
 
     if bar_width > 0:
         console.draw_rect(
-            x=0, y=45, width=bar_width, height=1, ch=1, bg=color.bar_empty
+            x=0, y=45, width=bar_width, height=1, ch=1, bg=color.bar_filled
         )
 
     console.print(
