a4921248f5b84d0e0516f62f77918d9464194917
nuzcraft <nuzcraft@gmail.com>
2022-03-26 17:11:28 -0500

    finalizing unit tests now that itemaction is a thing

--- Tests/test_consumable.py
+++ Tests/test_consumable.py
@@ -1,6 +1,13 @@
 import unittest
+from unittest.mock import patch
 from components.consumable import Consumable, HealingConsumable
-from entity import Item
+from components.ai import BaseAI
+from components.fighter import Fighter
+from entity import Item, Actor
+from actions import ItemAction
+from engine import Engine
+from game_map import GameMap
+from exceptions import Impossible
 
 
 class TestConsumable(unittest.TestCase):
@@ -17,15 +24,28 @@ class TestConsumable(unittest.TestCase):
     def test_get_action(self):
         '''
         test that get action returns the correct item action
-        TODO: implement this once item actions are implemented
         '''
+        actor = Actor(ai_cls=BaseAI, fighter=Fighter(hp=10, defense=10, power=10))
+        consumable = Consumable()
+        item = Item(consumable=consumable)
+        consumable.parent = item
+        action = consumable.get_action(consumer=actor)
+        self.assertIsInstance(action, ItemAction)
+        # ideally we'd check that the item action was created correctly
+        # but I'm not sure how :)
 
     def test_activate(self):
         '''
         test that for a base consumable this returns an not implemented error
-        TODO: implement this once item actions are implemented
         '''
+        actor = Actor(ai_cls=BaseAI, fighter=Fighter(hp=10, defense=10, power=10))
+        consumable = Consumable()
+        item = Item(consumable=consumable)
+        consumable.parent = item
+        action = consumable.get_action(consumer=actor)
 
+        with self.assertRaises(NotImplementedError):
+            consumable.activate(action)
 
 class TestHealingConsumable(unittest.TestCase):
     def test_init(self):
@@ -36,8 +56,41 @@ class TestHealingConsumable(unittest.TestCase):
         cm = HealingConsumable(amount=amt)
         self.assertEqual(cm.amount, amt)
 
-    def test_activate(self):
+    def test_activate_with_recovery(self):
         '''
         test that activate will heal the fighter
-        TODO: implment this once item actions are implemented
         '''
+        actor = Actor(ai_cls=BaseAI, fighter=Fighter(hp=10, defense=10, power=10))
+        actor.fighter.hp = 3
+        eng = Engine(player=actor)
+        gm = GameMap(engine=eng, width=10, height=10)
+        consumable = HealingConsumable(amount=5)
+        item = Item(consumable=consumable)
+        item.parent = gm
+        consumable.parent = item
+        action = consumable.get_action(consumer=actor)
+
+        with patch('message_log.MessageLog.add_message') as patch_add_message:
+            consumable.activate(action=action)
+
+        patch_add_message.assert_called_once()
+        self.assertEqual(actor.fighter.hp, 8)
+
+    def test_activate_without_recovery(self):
+        '''
+        test that activate raise Impossible when it cannot heal
+        '''
+        actor = Actor(ai_cls=BaseAI, fighter=Fighter(hp=10, defense=10, power=10))
+        eng = Engine(player=actor)
+        gm = GameMap(engine=eng, width=10, height=10)
+        consumable = HealingConsumable(amount=5)
+        item = Item(consumable=consumable)
+        item.parent = gm
+        consumable.parent = item
+        action = consumable.get_action(consumer=actor)
+
+        with self.assertRaises(Impossible):
+            consumable.activate(action=action)
+
+
+
