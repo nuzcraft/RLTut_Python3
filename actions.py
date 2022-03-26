b1a3cd798c9cb8ed6b123da17ea066cb2ebe4026
nuzcraft <nuzcraft@gmail.com>
2022-03-26 15:47:32 -0500

    added ItemAction

--- actions.py
+++ actions.py
@@ -7,7 +7,7 @@ import exceptions
 
 if TYPE_CHECKING:
     from engine import Engine
-    from entity import Entity, Actor
+    from entity import Entity, Actor, Item
 
 
 class Action:
@@ -115,3 +115,22 @@ class BumpAction(ActionWithDirection):
             return MeleeAction(self.entity, self.dx, self.dy).perform()
         else:
             return MovementAction(self.entity, self.dx, self.dy).perform()
+
+class ItemAction(Action):
+    def __init__(
+        self, entity: Actor, item: Item, target_xy: Optional[Tuple[int, int]] = None
+    ):
+        super().__init__(entity)
+        self.item = item
+        if not target_xy:
+            target_xy = entity.x, entity.y
+        self.target_xy = target_xy
+
+    @property
+    def target_actor(self) -> Optional[Actor]:
+        """return the actor at this action's destination"""
+        return self.engine.game_map.get_actor_at_location(*self.target_xy)
+
+    def perform(self) -> None:
+        """invoke the items ability, this action will be given to provide context"""
+        self.item.consumable.activate(self)
