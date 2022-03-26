320282cb5fd2bfebabb8277b1d6b2b4ffa0e7bb6
nuzcraft <nuzcraft@gmail.com>
2022-03-26 15:02:49 -0500

    adding unit test (incomplete)

--- Tests/test_consumable.py
+++ Tests/test_consumable.py
@@ -1,5 +1,5 @@
 import unittest
-from components.consumable import Consumable
+from components.consumable import Consumable, HealingConsumable
 from entity import Item
 
 
@@ -13,3 +13,31 @@ class TestConsumable(unittest.TestCase):
         item = Item(consumable=cm)
         cm.parent = item
         self.assertEqual(cm.parent, item)
+
+    def test_get_action(self):
+        '''
+        test that get action returns the correct item action
+        TODO: implement this once item actions are implemented
+        '''
+
+    def test_activate(self):
+        '''
+        test that for a base consumable this returns an not implemented error
+        TODO: implement this once item actions are implemented
+        '''
+
+
+class TestHealingConsumable(unittest.TestCase):
+    def test_init(self):
+        '''
+        test that a healing consumable amount initializes without issues
+        '''
+        amt = 5
+        cm = HealingConsumable(amount=amt)
+        self.assertEqual(cm.amount, amt)
+
+    def test_activate(self):
+        '''
+        test that activate will heal the fighter
+        TODO: implment this once item actions are implemented
+        '''
