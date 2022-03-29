caffa7063aad083c80a35071c509d200f59d2ad7
nuzcraft <nuzcraft@gmail.com>
2022-03-28 22:19:17 -0500

    added new classes for activating and dropping items

--- input_handlers.py
+++ input_handlers.py
@@ -4,6 +4,7 @@ from typing import Optional, TYPE_CHECKING
 
 import tcod
 
+import actions
 from actions import (
     Action,
     EscapeAction,
@@ -289,3 +290,29 @@ class InventoryEventHandler(AskUserEventHandler):
     def on_item_selected(self, item: Item) -> Optional[Action]:
         """Called when the user selects a valid item"""
         raise NotImplementedError()
+
+
+class InventoryActivateHandler(InventoryEventHandler):
+    """
+    Handle using an inventory item.
+    """
+    TITLE = "Select an item to use"
+
+    def on_item_selected(self, item: Item) -> Optional[Action]:
+        """
+        Return the action for the selected item
+        """
+        return item.consumable.get_action(self.engine.player)
+
+
+class InventoryDropHandler(InventoryEventHandler):
+    """
+    Handle dropping an inventory item
+    """
+    TITLE = "Select an item to drop"
+
+    def on_item_selected(self, item: Item) -> Optional[Action]:
+        """
+        Drop this item
+        """
+        return actions.DropItem(self.engine.player, item)
