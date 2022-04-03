4e52440763580a1afbc0db76cc3a6f4c790a4e02
nuzcraft <nuzcraft@gmail.com>
2022-04-03 07:17:26 -0500

    rendering the current dungeon level on screen

--- engine.py
+++ engine.py
@@ -57,6 +57,12 @@ class Engine:
             total_width=20,
         )
 
+        render_functions.render_dungeon_level(
+            console=console,
+            dungeon_level=self.game_world.current_floor,
+            location=(0, 47),
+        )
+
         render_functions.render_names_at_mouse_location(
             console=console, x=21, y=44, engine=self)
 
