c9ec15c21b5d17cc7865c907e33a6efec31233fb
nuzcraft <nuzcraft@gmail.com>
2022-04-04 14:42:01 -0500

    add level component to actor

--- entity.py
+++ entity.py
@@ -11,6 +11,7 @@ if TYPE_CHECKING:
     from components.consumable import Consumable
     from components.fighter import Fighter
     from components.inventory import Inventory
+    from components.level import Level
     from game_map import GameMap
 
 T = TypeVar("T", bound="Entity")
@@ -73,7 +74,7 @@ class Entity:
         """
         Return the distance between the current entity and the given (x, y) coordinate
         """
-        return math.sqrt((x- self.x) ** 2 + (y - self.y) ** 2)
+        return math.sqrt((x - self.x) ** 2 + (y - self.y) ** 2)
 
     def move(self, dx: int, dy: int) -> None:
         # move the entity by a given amount
@@ -93,6 +94,7 @@ class Actor(Entity):
         ai_cls: Type[BaseAI],
         fighter: Fighter,
         inventory: Inventory,
+        level: Level,
     ):
         super().__init__(
             x=x,
@@ -111,6 +113,9 @@ class Actor(Entity):
 
         self.inventory = inventory
         self.inventory.parent = self
+
+        self.level = level
+        self.level.parent = self
 
     @property
     def is_alive(self) -> bool:
