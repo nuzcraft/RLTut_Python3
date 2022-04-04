8652efed09e1e1d534845d8bd8e95b8e80ceb451
nuzcraft <nuzcraft@gmail.com>
2022-04-04 12:12:57 -0500

    defining some leveling funtions and properties

--- Tests/test_level.py
+++ Tests/test_level.py
@@ -1,6 +1,13 @@
 import unittest
+from unittest.mock import patch
 
 from components.level import Level
+from components.ai import BaseAI
+from components.fighter import Fighter
+from components.inventory import Inventory
+from engine import Engine
+from entity import Actor
+from game_map import GameMap
 
 class TestLevel(unittest.TestCase):
     def test_init(self):
@@ -13,3 +20,116 @@ class TestLevel(unittest.TestCase):
         self.assertEqual(lvl.level_up_base, 0)
         self.assertEqual(lvl.level_up_factor, 150)
         self.assertEqual(lvl.xp_given, 0)
+
+    def test_property_experience_to_next_level(self):
+        '''
+        test that the experience to next level property returns properly
+        '''
+        lvl = Level(
+            current_level=5,
+            level_up_base=200,
+            level_up_factor=150,
+        )
+        expected_return = 200 + 5*150
+        self.assertEqual(lvl.experience_to_next_level, expected_return)
+
+    def test_property_requires_level_up_true(self):
+        '''
+        test that the requires level up property will return true when current xp
+        is greater than the experience needed for the next level
+        '''
+        lvl = Level(
+            current_level=1,
+            current_xp=1000,
+            level_up_base=200,
+            level_up_factor=150,
+        )
+        self.assertTrue(lvl.requires_level_up)
+
+    def test_property_requires_level_up_false(self):
+        '''
+        test that the requires level up property will return true when current xp
+        is greater than the experience needed for the next level
+        '''
+        lvl = Level(
+            current_level=1,
+            current_xp=100,
+            level_up_base=200,
+            level_up_factor=150,
+        )
+        self.assertFalse(lvl.requires_level_up)
+
+    def test_add_xp_no_xp(self):
+        '''
+        test that calling add_xp adding 0 xp will not add xp
+        '''
+        lvl = Level(current_xp=0, level_up_base=200)
+        lvl.add_xp(xp=0)
+        self.assertEqual(lvl.current_xp, 0)
+
+    def test_add_xp_no_level_up_base(self):
+        '''
+        test that calling add_xp adding xp while level up base is 0 will not add xp
+        '''
+        lvl = Level(current_xp=0, level_up_base=0)
+        lvl.add_xp(xp=100)
+        self.assertEqual(lvl.current_xp, 0)
+
+    def test_add_xp(self):
+        '''
+        test that calling add_xp adding xp will also add a message to the log
+        '''
+        actor = Actor(
+            ai_cls=BaseAI,
+            fighter=Fighter(hp=10, defense=10, power=10),
+            inventory=Inventory(capacity=5)
+        )
+        eng = Engine(player=actor)
+        gm = GameMap(engine=eng, width=10, height=10)
+        actor.parent = gm
+        lvl = Level(
+            current_level=1,
+            level_up_base=200,
+            level_up_factor=150,
+        )
+        lvl.parent=actor
+
+        with patch('message_log.MessageLog.add_message') as patch_add_message:
+            lvl.add_xp(xp=100)
+
+        self.assertEqual(lvl.current_xp, 100)
+        patch_add_message.assert_any_call(
+            f"You gain 100 experience points."
+        )
+
+    def test_add_xp_with_level_up(self):
+        '''
+        test that calling add_xp adding xp will also add a message to the log
+        when the 
+        '''
+        actor = Actor(
+            ai_cls=BaseAI,
+            fighter=Fighter(hp=10, defense=10, power=10),
+            inventory=Inventory(capacity=5)
+        )
+        eng = Engine(player=actor)
+        gm = GameMap(engine=eng, width=10, height=10)
+        actor.parent = gm
+        lvl = Level(
+            current_level=1,
+            level_up_base=200,
+            level_up_factor=150,
+        )
+        lvl.parent=actor
+
+        with patch('message_log.MessageLog.add_message') as patch_add_message:
+            lvl.add_xp(xp=1000)
+
+        self.assertEqual(lvl.current_xp, 1000)
+        patch_add_message.assert_any_call(
+            f"You gain 1000 experience points."
+        )
+        patch_add_message.assert_any_call(
+            f"You advance to level 2!"
+        )
+
--- components/level.py
+++ components/level.py
@@ -23,3 +23,24 @@ class Level(BaseComponent):
         self.level_up_base = level_up_base
         self.level_up_factor = level_up_factor
         self.xp_given = xp_given
+
+    @property
+    def experience_to_next_level(self) -> int:
+        return self.level_up_base + self.current_level * self.level_up_factor
+
+    @property
+    def requires_level_up(self) -> bool:
+        return self.current_xp > self.experience_to_next_level
+
+    def add_xp(self, xp: int) -> None:
+        if xp == 0 or self.level_up_base == 0:
+            return
+
+        self.current_xp += xp
+
+        self.engine.message_log.add_message(f"You gain {xp} experience points.")
+
+        if self.requires_level_up:
+            self.engine.message_log.add_message(
+                f"You advance to level {self.current_level + 1}!"
+            )
