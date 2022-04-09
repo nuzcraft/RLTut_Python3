12c8fc17b1771168e90f44183bcbc7db42ddb395
nuzcraft <nuzcraft@gmail.com>
2022-04-08 20:32:39 -0500

    update actions for optional consumable

--- actions.py
+++ actions.py
@@ -169,7 +169,8 @@ class ItemAction(Action):
 
     def perform(self) -> None:
         """invoke the items ability, this action will be given to provide context"""
-        self.item.consumable.activate(self)
+        if self.item.consumable:
+            self.item.consumable.activate(self)
 
 
 class DropItem(ItemAction):
